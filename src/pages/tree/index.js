6a12704c87881a68a7a7b47ef65477bb89c07221
Rick1196 <ricardompp11@gmail.com>
2024-02-06 21:57:11 -0600

    Add element to tree

--- src/pages/tree/index.js
+++ src/pages/tree/index.js
@@ -1,40 +1,78 @@
+let root = undefined;
+const initializeTree = (value) => {
+  root = { value, leftChild: undefined, rightChild: undefined };
+};
+const addLeaf = (value, localRoot) => {
+  if (localRoot.value > value) {
+    // left
+    if (localRoot.leftChild) {
+      addLeaf(value, localRoot.leftChild);
+    } else {
+      localRoot.leftChild = {
+        value,
+        leftChild: undefined,
+        rightChild: undefined,
+      };
+      return;
+    }
+  } else {
+    // right
+    if (localRoot.rightChild) {
+      addLeaf(value, localRoot.rightChild);
+    } else {
+      localRoot.rightChild = {
+        value,
+        leftChild: undefined,
+        rightChild: undefined,
+      };
+      return;
+    }
+  }
+};
+const addElement = (value) => {
+  if (!root) {
+    initializeTree(value);
+  } else {
+    addLeaf(value, root);
+  }
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  function setFormListener(){
+  function setFormListener() {
     const form = document.getElementById("form");
     form.addEventListener("submit", (e) => {
       e.preventDefault();
       const formData = new FormData(e.target);
       const formValues = Object.fromEntries(formData);
-      console.log(formValues);
+      addElement(formValues["input"]);
+      console.log(root);
     });
-  };
-  function numberValidation(){
+  }
+  function numberValidation() {
     const input = document.getElementById("input");
-    input.addEventListener("input", (e) =>{
-        const regex = new RegExp("^[0-9]+$|^[0-9]+(.[0-9]+)$")
-        const value = e.target.value
-        console.log(value)
-        if(regex.test(value)){
-            console.log("ok!")
-            const error = document.getElementById("error");
+    input.addEventListener("input", (e) => {
+      const regex = new RegExp("^[0-9]+$|^[0-9]+(.[0-9]+)$");
+      const value = e.target.value;
+      console.log(value);
+      if (regex.test(value)) {
+        console.log("ok!");
+        const error = document.getElementById("error");
 
-            error && document.getElementById("label").removeChild(error);
-            document.getElementById("input").classList.remove("error")
-            document.getElementById("submit").setAttribute("disabled", false)
-
-        }else{
-            console.log("nah!")
-            const label = document.getElementById("label");
-            const error = document.createElement("p");
-            error.setAttribute("id", "error")
-            error.innerHTML = "Not a valid number!!"
-            label.appendChild(error);
-            document.getElementById("input").classList.add("error")
-            document.getElementById("submit").setAttribute("disabled", true)
-        }
-    })
+        error && document.getElementById("label").removeChild(error);
+        document.getElementById("input").classList.remove("error");
+        document.getElementById("submit").removeAttribute("disabled");
+      } else {
+        console.log("nah!");
+        const label = document.getElementById("label");
+        const error = document.createElement("p");
+        error.setAttribute("id", "error");
+        error.innerHTML = "Not a valid number!!";
+        label.appendChild(error);
+        document.getElementById("input").classList.add("error");
+        document.getElementById("submit").setAttribute("disabled", true);
+      }
+    });
   }
   setFormListener();
-  numberValidation()
+  numberValidation();
 });
